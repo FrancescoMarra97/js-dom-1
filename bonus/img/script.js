1dd18bc7fb37db29bbcc96d8135bbb54c8456966
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-08 19:03:46 +0200

    funzione sul controllo dello stato della lampadina

---
+++ bonus/img/script.js
@@ -0,0 +1,13 @@
+/* Fscciamo accendi e spegni
+Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni" 
+Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+e così via    */
+
+//seleziono gli elementi del DOM
+
+const lampadinaEl = document.getElementById("lampadina");
+const buttonEl = document.getElementById("button");
+
+function switchStatus() {
+    
+}
