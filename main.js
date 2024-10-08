6d95e30a53966c64b7611e36d4592379d0fd4321
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-08 14:06:09 +0200

    fix names

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // seleziono gli elementi del DOM
 
-const lampadina = document.getElementById
-const button = document.getElementById
+const lampadinaEl = document.getElementById
+const buttonEl = document.getElementById
+
+
